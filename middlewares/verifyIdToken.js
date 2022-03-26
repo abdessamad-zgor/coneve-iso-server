b10bf900baba6d59371fb9c9d12f2679cd556ec5
abdessamad-zgor <abdessamadzgor@gmail.com>
2022-03-26 22:53:31 +0100

    changing verifyIdToken middleware to access token from Authorization header

--- middlewares/verifyIdToken.js
+++ middlewares/verifyIdToken.js
@@ -1,18 +1,15 @@
 const { auth } = require('../config');
 
 const verifyIdToken = async (req, res, next) => {
-	try {
-		let response = await auth.verifyIdToken(req.body.idToken);
-		console.log(req.body.idToken);
-
-		if (response.exp > Date.now()) {
-			next();
-		} else {
-			new Error('auth token expired');
-		}
-	} catch (error) {
-		res.status(403).json({ status: 'error', message: 'Not Autherized!' });
-	}
+  try {
+    const idToken = req.headers.authorization.split(' ')[1];
+    let response = await auth.verifyIdToken(idToken);
+    console.log(req.headers);
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(403).json({ status: 'error', message: 'Not Autherized!' });
+  }
 };
 
 module.exports = verifyIdToken;
